7ac52aa0c2b57c8c9921fd2eec27c620007ef8f8
Svetlana <svetlana.moiseienko.web@gmail.com>
2023-03-14 12:28:45 +0100

    feat: bind the weather card to API data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,44 @@
+import { useState } from "react";
+import { WEATHER_API_KEY, WEATHER_API_URL } from "./api/api";
 import "./App.css";
 import CurrentWeather from "./components/CurrentWeather/CurrentWeather";
 import Search from "./components/search/Search";
 
 function App() {
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [forecast, setForecast] = useState(null);
+
   const onSearchChange = (searchData) => {
-    console.log(searchData);
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(
+      `${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+    const forecastFetch = fetch(
+      `${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`
+    );
+
+    Promise.all([currentWeatherFetch, forecastFetch])
+      .then(async (response) => {
+        const currentWeatherResponse = await response[0].json();
+        const forecastResponse = await response[1].json();
+
+        setCurrentWeather({
+          city: searchData.label,
+          ...currentWeatherResponse,
+        });
+        setForecast({ city: searchData.label, ...forecastResponse });
+      })
+      .catch((err) => console.log(err));
   };
+
+  console.log(currentWeather);
+  console.log(forecast);
 
   return (
     <div className="container">
       <Search onSearchChange={onSearchChange} />
-      <CurrentWeather />
+      {currentWeather && <CurrentWeather data={currentWeather} />}
     </div>
   );
 }
--- src/components/CurrentWeather/CurrentWeather.jsx
+++ src/components/CurrentWeather/CurrentWeather.jsx
@@ -1,37 +1,40 @@
 import React from "react";
 import styles from "./CurrentWeather.module.css";
-import sunny from "../../img/icons/01d.png";
 
-const CurrentWeather = () => {
+const CurrentWeather = ({ data }) => {
+  const numsRound = (num) => {
+    return num > Math.trunc(num) ? num.toFixed(1) : num;
+  };
+
   return (
     <div className={styles.card}>
       <div className={styles.header}>
         <div className={styles.mainInfo}>
-          <h3>London</h3>
-          <span>sunny</span>
+          <h3>{data.name}</h3>
+          <span>{data.weather[0].description}</span>
         </div>
-        <img src={sunny} alt="weather-icon" />
+        <img src={`img/icons/${data.weather[0].icon}.png`} alt="weather-icon" />
       </div>
       <div className={styles.body}>
-        <h2 className={styles.deg}>15°C</h2>
+        <h2 className={styles.deg}>{Math.round(data.main.temp)}°C</h2>
         <div className={styles.details}>
           <div className={styles.title}>Details</div>
           <div className={styles.info}>
             <div className={styles.item}>
               <div>Feels like</div>
-              <span>11°C</span>
+              <span>{Math.round(data.main.feels_like)}°C</span>
             </div>
             <div className={styles.item}>
               <div>Wind</div>
-              <span>4 m/s</span>
+              <span>{numsRound(data.wind.speed)} m/s</span>
             </div>
             <div className={styles.item}>
               <div>Humidity</div>
-              <span>15%</span>
+              <span>{data.main.humidity}%</span>
             </div>
             <div className={styles.item}>
               <div>Pressure</div>
-              <span>10 hPa</span>
+              <span>{data.main.pressure} hPa</span>
             </div>
           </div>
         </div>
